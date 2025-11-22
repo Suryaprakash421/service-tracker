3a251a500b8f610c2b027b33659a4b151d41eeeb
Suryaprakash Subramani <suryaprakash.s@comviva.com>
2025-11-22 16:36:20 +0530

    feat: implement theme toggle functionality and integrate ThemeProvider

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ThemeToggle } from "./ui/theme-toggle";
 
 export function SiteHeader() {
   return (
@@ -23,8 +24,9 @@ export function SiteHeader() {
               GitHub
             </a>
           </Button>
+          <ThemeToggle />
         </div>
       </div>
     </header>
-  )
+  );
 }
